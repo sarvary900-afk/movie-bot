cd3e9382c2cd3baa08f856b4b876b6f2b489b081
sarvary900-afk <sarvary900@gmail.com>
2026-03-04 23:11:48 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,30 +9,20 @@ if not TOKEN:
 bot = telebot.TeleBot(TOKEN)
 
 
+# 🎬 Kino bazasi
+movies = {
+    "777": "BAACAgEAAxkBAAMGaahynCZPu32jml-N6gr35uXbuaEAAqQJAAImbzFFwH61tu76gxQ6BA"
+}
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, "Salom! Kino kodini yuboring 🎬")
 
 
-# 🔹 FILE_ID olish
-@bot.message_handler(content_types=['video', 'document'])
-def get_file_id(message):
-    if message.video:
-        file_id = message.video.file_id
-    else:
-        file_id = message.document.file_id
-
-    bot.reply_to(message, f"FILE_ID:\n{file_id}")
-
-
-# 🔹 Kod orqali kino yuborish
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def send_movie(message):
-    code = message.text
-
-    movies = {
-        "100": "BU_YERGA_FILE_ID"
-    }
+    code = message.text.strip()
 
     if code in movies:
         bot.send_video(message.chat.id, movies[code])
