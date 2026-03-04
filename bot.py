fe54468e0ec68c828d73f4db9ddfc8cd491e4037
sarvary900-afk <sarvary900@gmail.com>
2026-03-04 15:43:25 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+import os
+import telebot
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Salom! Kino kodini yuboring 🎬")
+
+@bot.message_handler(func=lambda message: True)
+def send_movie(message):
+    code = message.text
+    
+    movies = {
+        "100": "Bu yerga kino linkini qo'yasiz",
+        "200": "Bu yerga boshqa kino linki"
+    }
+    
+    if code in movies:
+        bot.reply_to(message, movies[code])
+    else:
+        bot.reply_to(message, "Bunday kod topilmadi ❌")
+
+bot.polling()
