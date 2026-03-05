abf9b90a2f505c6c1d3ff82696b0852e6c209105
sarvary900-afk <sarvary900@gmail.com>
2026-03-05 17:04:29 +0500

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,1 +1,13 @@
+from config import ADMIN_ID
+from database import sql
+from users import get_users
 
+def is_admin(user):
+
+    return user == ADMIN_ID
+
+
+def user_count():
+
+    sql.execute("SELECT COUNT(*) FROM users")
+    return sql.fetchone()[0]
