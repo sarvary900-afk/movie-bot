be3d7d112653f94819bb8ad6ee2dd32fce1f8591
sarvary900-afk <sarvary900@gmail.com>
2026-03-05 16:57:38 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,24 @@
+import sqlite3
 
+db = sqlite3.connect("kino.db", check_same_thread=False)
+sql = db.cursor()
+
+sql.execute("""
+CREATE TABLE IF NOT EXISTS movies(
+code TEXT,
+name TEXT,
+genre TEXT,
+file_id TEXT,
+views INTEGER,
+premium INTEGER
+)
+""")
+
+sql.execute("""
+CREATE TABLE IF NOT EXISTS users(
+user_id INTEGER,
+ref INTEGER
+)
+""")
+
+db.commit()
