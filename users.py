c7aae9cca94398b1c1a3e0300522263b37ff4d07
sarvary900-afk <sarvary900@gmail.com>
2026-03-05 17:01:59 +0500

    Update users.py

--- users.py
+++ users.py
@@ -1,1 +1,16 @@
+from config import CHANNELS
+from database import sql, db
 
+def add_user(user, ref):
+
+    sql.execute("SELECT user_id FROM users WHERE user_id=?", (user,))
+    if not sql.fetchone():
+
+        sql.execute("INSERT INTO users VALUES (?,?)", (user, ref))
+        db.commit()
+
+
+def get_users():
+
+    sql.execute("SELECT user_id FROM users")
+    return sql.fetchall()
