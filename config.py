e55366e2151c732a1aae108ce36969ce10731d3b
sarvary900-afk <sarvary900@gmail.com>
2026-03-15 22:56:13 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
-TOKEN = "8288853849:AAFfJaYec6NgTldlZp2R7svby5iSLc8lB8E"
+# config.py (GitHub uchun yangilangan versiya)
 
+# Bot tokeni
+BOT_TOKEN = "8288853849:AAFfJaYec6NgTldlZp2R7svby5iSLc8lB8E"
+
+# Admin ID
 ADMIN_ID = 6401247171
 
-CHANNELS = [
-"@trend_muzikalar_uz_01",
-"@Dramalar_olami_uzz",
-"@daxshat_kinolar_uzzz"
+# Kanallar ro'yxati (foydalanuvchi obuna bo‘lishi kerak)
+KANALLAR = [
+    {"name": "Trend Musikalar UZ", "url": "https://t.me/trend_muzikalar_uz_01"},
+    {"name": "Dramalar Olami UZZ", "url": "https://t.me/Dramalar_olami_uzz"},
+    {"name": "Daxshat Kinolar UZZZ", "url": "https://t.me/daxshat_kinolar_uzzz"}
 ]
-
-UPLOAD_CHANNEL = "@daxshat_kinolar_uzzz"
