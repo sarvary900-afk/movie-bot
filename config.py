18b8188e0bec9c29b66074b35e298a638a71f7c1
sarvary900-afk <sarvary900@gmail.com>
2026-03-15 23:07:45 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 BOT_TOKEN = "8288853849:AAFfJaYec6NgTldlZp2R7svby5iSLc8lB8E"
-
-# Admin ID
 ADMIN_ID = 6401247171
 
-# Kanallar ro'yxati (foydalanuvchi obuna bo‘lishi kerak)
 KANALLAR = [
     {"name": "Trend Musikalar UZ", "url": "https://t.me/trend_muzikalar_uz_01"},
     {"name": "Dramalar Olami UZZ", "url": "https://t.me/Dramalar_olami_uzz"},
